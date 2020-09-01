c8922583d7cef958e8187dfe2829a3e9ffde1d9e
ones-and-zeroes <shreyansh.yadav.cse19@itbhu.ac.in>
2020-09-01 05:52:39 +0530

    Created basic appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:covidvaccineapp/ui/HomeScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(App());
@@ -7,15 +9,20 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarDividerColor: Colors.grey,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Covid Vaccine App"),
-        ),
-        body: Center(
-          child: Text("Covid Vaccine App"),
-        ),
-      ),
+      title: 'Covid App',
+      debugShowCheckedModeBanner: false,
+//      theme: ThemeData(
+//        TODO: set up theme data
+//      ),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/ui/HomeScreen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: Icon(Icons.dehaze),
+        title: Text("Covid app"),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          ),
+        ),
+      ),
+    );
+  }
+}
