4a0ca3baf90e50c45db0cfab7ee8bfb487df2b0f
ones-and-zeroes <shreyansh.yadav.cse19@itbhu.ac.in>
2020-09-11 18:18:00 +0530

    Bug fix: renaming file import

--- lib/screens/login_screen/SignInOne.dart
+++ lib/screens/login_screen/SignInOne.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '../../Home.dart';
 import '../../sign_up.dart';
 // import './SignUpScreen.dart';
-import '../../Widgets/login/already_have_a_account_check.dart';
+import '../../widgets/login/already_have_a_account_check.dart';
 
 class SignInOne extends StatelessWidget {
   final _passwordFocusNode = FocusNode();
