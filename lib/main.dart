adb705fc32d7246670c7e0ab35de6294e82a5a65
shreyanshyad <shreyansh.yadav.cse19@itbhu.ac.in>
2020-09-16 19:22:05 +0530

    Implemented user details page and form 1 of 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:covidvaccineapp/screens/UserDetails.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -8,6 +10,7 @@ import './screens/HomeScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await SystemChrome.setPreferredOrientations(<DeviceOrientation>[
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown
