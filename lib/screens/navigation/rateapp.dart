12b0c6de8be5dbd0bac77198d452935effbd9113
Saumay Gupta <saumay.gupta.cse19@itbhu.ac.in>
2020-10-21 17:51:59 +0530

    removed redundent files

--- lib/screens/navigation/rateapp.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-// import 'package:share/share.dart';
-
-class Rateapp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog();
-  }
-}
